ea5c7c73a79bdc78c04e970a18139275da5075f8
rjcdevel@gmail.com <rjcdevel@gmail.com>
2020-10-21 10:23:15 -0400

    added functionality to findDefaultPages.py

--- findDefaultPages.py
+++ findDefaultPages.py
@@ -5,32 +5,46 @@ import getopt, sys
 import os.path
 from os import path
 
-words = {
-    b'Apache2 server after installation on Debian systems' : 'Default Webpage Found' + "\t" + 'Apache2 on Debian/Ubuntu',
-    b'Apache2 server after installation on Ubuntu systems' : 'Default Webpage Found' + "\t" + 'Apache2 on Debian/Ubuntu',
-    b'Apache 2 Test Page' : 'Default Webpage Found' + "\t" + 'Apache 2 running',
-    b'Welcome to nginx' : 'Default Webpage Found' + "\t" + 'Nginx Unknown Version',
-    b'Apache Server Status' : 'Default Webpage Found' + "\t" + 'Apache Unknown Version',
-    b'Welcome to the Advanced Extranet Server, ADVX!' : 'Default Webpage Found'  + "\t" + 'ADVX ',
-    b'Fedora Core Test Page' : 'Default Webpage Found' + "\t" + 'Outdated Apache 2.0 on Fedora',
-    b'Apache HTTP Server on Fedora Core'  : 'Default Webpage Found' + "\t" + 'Outdated Apache 2.0 on Fedora',
-    b'xampp/index'  : 'Default Webpage Found' + "\t" + 'Xampp',
-    b'Apache/2.0.* (Linux/SuSE)' : 'Default Webpage Found' + "\t" + 'Outdated Apache2.0 on Linux SuSE' ,
-    b'Test Page for Apache' : 'Default Webpage Found  ' + "\t" + 'Outdated Apache Unknown Version',
-    b'404 Object Not Found' :  'Default Webpage Found  ' + "\t" + 'Outdated IIS5.0',
-    b'Microsoft-IIS/5.0 server at'  : 'Default Webpage Found  ' + "\t" + 'Outdated IIS5.0' ,
-    b'index.of'  : 'Default Webpage Found  ' + "\t" + 'IIS Unknow Version',
-    b'alt="IIS7"'  : 'Default Webpage Found  ' + "\t" + 'IIS7',
-    b'Welcome.png'  : 'Default Webpage Found  ' + "\t" + 'IIS Unknown Version',
-    b'The initial installation of Debian/GNU Apache' : 'Default Webpage Found  ' + "\t" + 'Apache Unknown Version'
+stats = {'404s' : 0,
+         'Timeouts' : 0,
+         'Founds' : 0,
+         'Untracked' : 0,
+         'Not Founds': 0}
+
+# set list of search phrases that indicate default web pages from well known servers
+terms = {
+    # debian/ubuntu apache
+    b'Apache2 server after installation on Debian systems' : 'Default page Found' + "\t" + 'Apache2 on Debian/Ubuntu',
+    b'The initial installation of Debian/GNU Apache' : 'Default page found  ' + "\t" + 'Apache Unknown Version',
+    # apache
+    b'Apache2 server after installation on Ubuntu systems' : 'Default page Found' + "\t" + 'Apache2 on Debian/Ubuntu',
+    b'Apache 2 Test Page' : 'Default page found' + "\t" + 'Apache 2 running',
+    b'Apache Server Status' : 'Default page found' + "\t" + 'Apache Unknown Version',
+    b'Apache HTTP Server on Fedora Core'  : 'Default page found' + "\t" + 'Outdated Apache 2.0 on Fedora',
+    b'Fedora Core Test Page' : 'Default page found' + "\t" + 'Outdated Apache 2.0 on Fedora',
+    b'Apache/2.0.* (Linux/SuSE)' : 'Default page found' + "\t" + 'Outdated Apache2.0 on Linux SuSE' ,
+    b'xampp/index'  : 'Default page found' + "\t" + 'Xampp',
+    b'Test Page for Apache' : 'Default page found  ' + "\t" + 'Outdated Apache Unknown Version',
+    # nginx
+    b'Welcome to nginx' : 'Default page found' + "\t" + 'Nginx Unknown Version',
+    # iis
+    b'404 Object Not Found' :  'Default page found  ' + "\t" + 'Outdated IIS5.0',
+    b'index.of'  : 'Default page found  ' + "\t" + 'IIS Unknow Version',
+    b'Microsoft-IIS/5.0 server at'  : 'Default page found  ' + "\t" + 'Outdated IIS5.0' ,
+    b'alt="IIS7"'  : 'Possible Default page found  ' + "\t" + 'Likely IIS7',
+    b'Welcome.png'  : 'Possible Default page found  ' + "\t" + 'Likely IIS Unknown Version',
+    # advx
+    b'Welcome to the Advanced Extranet' : 'Default page found'  + "\t" + 'ADVX '
 }
 
+# get command line arguments
 def setOpts():
 
-    global ifile
+    global ifile, reverse
     ifile = False
-    flags = "hf:"
-    longflags = ["help", "file"]
+    reverse = False
+    flags = "hrf:"
+    longflags = ["help", "reverse", "file"]
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], flags, longflags)
@@ -48,6 +62,8 @@ def setOpts():
             # if file provided is not an actual file
             if not path.isfile(ifile):
                 assert False, "ERROR: invalid file provided to -f or --file argument; for usage info python findDefaultPages.py -h"
+        elif o in ("-r", "--reverse"):
+            reverse = True
         else:
             assert False, "ERROR: unhandled option; for usage info python findDefaultPages.py -h"
 
@@ -55,16 +71,24 @@ def setOpts():
         assert False, "ERROR: no file provided via -f or --file argument; for usage info python findDefaultPages.py -h"
 
 
+# print usage message 
 def usage(ecode):
     print()
     print('usage: python findDefaultPages.py -h or python findDefaultPages.py -f [file]')
+    print()
     print('     -h --help:     display help info')
+    print('     -r --reverse:  only print URLs or IP addresses where content was found but not default webserver pages')
     print('     -f --file:     path to file containing urls or ip addresses to query for default webserver pages')
     print()
     sys.exit(ecode)
 
 def search():
-    global words
+    print()
+    print()
+    print("****************** START ****************************")
+    print()
+    print()
+    global terms, stats 
     protocols = ['http','https'] 
     url = '' 
 
@@ -81,23 +105,52 @@ def search():
                 req = urllib.request.Request(url=url,headers=headers)
                 resp = urllib.request.urlopen(req, timeout=3)
             except timeout:
-                print('Timeout error against url: ' + str(url))
+                stats['Timeouts'] += 1
+                if not reverse:
+                    print('Timeout error against url: ' + str(url))
                 continue
             except Exception as e:
-                print(str(url) + "\t" + str(e))
+                if hasattr(e,'code') and e.code == 404:
+                    stats['404s'] += 1
+                else:
+                    stats['Untracked'] += 1
+                if not reverse:
+                    print(str(url) + "\t" + str(e))
                 continue
 
             # check contents for particular content
             # use > -1 to check for content to exist
             # use == -1 t check for content not existing
             contents = resp.read()
-            for w in words:
-                if contents.find(w)>-1:
-                    print(url + "\t" + words[w])
+            found = False
+            for t in terms:
+                if contents.find(t)>-1:
+                    if not reverse:
+                        print(url + "\t" + terms[t])
+                    stats['Founds'] += 1
+                    found = True
+                    break 
+            if not found:
+                print(url + "\t Content found not likely default pages")
+                stats['Not Founds'] += 1
+    print()
+    print()
+    print("******************* END *****************************")
 
+def printStats():
+    print('Script encountered ' + str(stats['404s']) + ' 404 error(s)')
+    print('Script encountered ' + str(stats['Timeouts']) + ' Connection Timeout(s)')
+    print('Script disovered ' + str(stats['Founds']) + ' likely webserver default pages')
+    print('Script disovered ' + str(stats['Not Founds']) + ' web pages that are likely not default pages')
+    print('Script encountered ' + str(stats['Untracked']) + ' URL(s) or IP(s) which failed to load due to untracked errors.')
+    print()
+    print('To confirm content not likelyto be default pages, run script with -r flag to reverse the functionality. This will only list results where content was found but default pages were not found. You will need to manually check these results.')
+    print()
+    
 def main():
     setOpts()
     search()
+    printStats()
     sys.exit()
 
 if __name__ == "__main__":
