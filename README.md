60856d77c6469cb77afe53a387eb99c75688968e
rjcdevel@gmail.com <rjcdevel@gmail.com>
2020-10-20 20:16:52 -0400

    added createAttack.py

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 ## findDefaultPages.py 
 
-Python script that accepts a file as input which should contain one IP address or one URL per line
+Python3 script that accepts a file as input which should contain one IP address or one URL per line
 
 For each line in the provided file, this script will:
 
@@ -23,4 +23,36 @@ usage: python findDefaultPages.py -h or python findDefaultPages.py -f [file]
      -h --help:     display help info
      -f --file:     path to file containing urls or ip addresses to query for default webserver pages
 ```
+## createAttack.py
 
+Python3 script that generates the beginning of an attack script against the url provided. The script uses attack.py.tmpl, replacing variables in the tmpl file with values provided in call to createAttack.py. Once script is created, user should open new script and complete the attack code as outlined in the new script. 
+
+The benefit of using createAttack.py is import statements are written, url request code is written, among other things. User doesn't need to write from scratch.
+
+```
+usage: createAttack.py -h
+       createAttack.py -u [url] -s [scriptname.py]
+
+     -h --help:     display help info
+
+     -s --script:   required name of script to create
+     -u --url:      required url of target site or page ie www.somesite.com
+     -p --protocol: protocol ie http, ftp, https
+     -o --port:     port ie 80, 443, 8080
+     -c --cookie:   cookie value ie PHPSESSID=XXX999XXX
+     -a --auth:     value of Authorization header
+
+Example usage creates a scipt called attack.py:
+
+     createAttack.py -u 'www.someurl.com' \
+                      -p 'http'  \
+                      -a 'Basic bmSomeBasicAuthStringGoesHere9999XXXXXN0TlZrbXhkazM5Sg==' \
+                      -s attack.py
+
+     createAttack.py --url 'www.someurl.com' \
+                      --protocol 'http'  \
+                      --auth 'Basic bmSomeBasicAuthStringGoesHere9999XXXXXN0TlZrbXhkazM5Sg==' \
+                      --script attack.py
+
+
+```
---
+++ createAttack.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+import fileinput
+from shutil import copyfile
+import getopt, sys
+import re
+import os
+
+protocol = 'http' 
+url = None
+port = '' 
+
+cookie =  ''
+auth = '' 
+verbose = '' 
+
+script = None
+
+def setOpts():
+    global protocol, url, port, cookie, auth, script
+    flags = "vhp:u:o:c:a:s:"
+    longflags = ["help", "protocol=", "url=", "port=", "cookie=", "auth=", "script="]
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], flags, longflags)
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(str(err))  # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    optsSet = True
+    for o, a in opts:
+        if o == "-v":
+            verbose = True
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-p", "--protocol"):
+            if a not in ('ftp', 'http', 'https'):
+                print("Invalid protocol. Valid values are ftp, http, https.")
+                optsSet = False
+            else:
+                protocol = a
+        elif o in ("-u", "--url"):
+            url = a
+            if re.match(r'^[.a-zA-Z0-9]+$', url) is None:
+                print('Malformed url: ' + str(url))
+                optsSet = False
+        elif o in ("-o", "--port"):
+            port = a
+            if re.match(r'^[0-9]+$', port) is None:
+                print('Invalid port: ' + port)
+                optsSet = False
+        elif o in ("-c", "--cookie"):
+            cookie = a
+            if re.match(r'^[\s;:=a-zA-Z0-9]+$', cookie) is None:
+                print('Malformed cookie: can contain spaces ; : = a-zA-Z0-9')
+                optsSet = False
+        elif o in ("-a", "--auth"):
+            auth = a
+            if re.match(r'^[\s;:=a-zA-Z0-9]+$', auth) is None:
+                print('Malformed auth value: can contain spaces ; : = a-zA-Z0-9')
+                optsSet = False
+        elif o in ("-s", "--script"):
+            script = a
+            if re.match(r'^[a-zA-Z0-9]+\.py$', script) is None:
+                print('Malformed script name: ^[a-zA-Z0-9]+\.py$')
+                optsSet = False
+            if os.path.isfile(script):
+                print('Cannot create  ' + str(script) + ' - already exists.')
+                optsSet = False
+        else:
+            assert False, "unhandled option"
+
+    if url == None or script == None:
+        usage()
+        print('Note: url and script are required using -u or --url and -s or --script')
+        print('')
+        optsSet = False
+
+    if optsSet is False:
+        sys.exit(2)
+
+def main():
+    setOpts();
+
+    # copy attack.py template to file with requested script name 
+    copyfile(os.path.dirname(os.path.realpath(__file__)) + '/attack.py.tmpl', script)
+
+    # read each line from newly created script and replace variables
+    for line in fileinput.input(script, inplace=True):
+
+        if '%%PROTO%%' in line:
+            print(line.replace('%%PROTO%%', protocol), end="")
+
+        elif '%%URL%%' in line:
+            print(line.replace('%%URL%%', url), end="")
+
+        elif '%%PORT%%' in line:
+            print(line.replace('%%PORT%%', port), end="")
+
+        elif '%%COOKIE%%' in line:
+            print(line.replace('%%COOKIE%%', cookie), end="")
+
+        elif '%%AUTH%%' in line:
+            print(line.replace('%%AUTH%%', auth), end="")
+
+        else:
+            print(line, end="")
+
+def usage():
+    print('')
+    print('usage: createAttack.py -h')
+    print('       createAttack.py -u [url] -s [scriptname.py]')
+    print('')
+    print('     -h --help:     display help info')
+    print('')
+    print('     -s --script:   required name of script to create')
+    print('     -u --url:      required url of target site or page ie www.somesite.com')
+    print('     -p --protocol: protocol ie http, ftp, https')
+    print('     -o --port:     port ie 80, 443, 8080')
+    print('     -c --cookie:   cookie value ie PHPSESSID=XXX999XXX')
+    print('     -a --auth:     value of Authorization header')
+    print('')
+    print('Example usage creates a scipt called attack.py:') 
+    print('')
+    print("     createAttack.py -u 'www.someurl.com' \\")
+    print("                      -p 'http'  \\")
+    print("                      -a 'Basic bmSomeBasicAuthStringGoesHere9999XXXXXN0TlZrbXhkazM5Sg==' \\")
+    print("                      -s attack.py") 
+    print('')
+    print("     createAttack.py --url 'www.someurl.com' \\")
+    print("                      --protocol 'http'  \\")
+    print("                      --auth 'Basic bmSomeBasicAuthStringGoesHere9999XXXXXN0TlZrbXhkazM5Sg==' \\")
+    print("                      --script attack.py") 
+    print('')
+
+if __name__ == "__main__":
+    main()
